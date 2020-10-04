3924ccb649dff52f2e255df0380c5e2c540da441
Eigilak <48329669+Eigilak@users.noreply.github.com>
2020-10-04 15:11:05 +0200

    Update GoogleLogin.js

--- GoogleLogin.js
+++ GoogleLogin.js
@@ -1,7 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button, Alert,LogBox } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+
+
+
 /*config indstillinger start */
 
     iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
     androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
     iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
     androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>'
+/* slut */
+
+
 /**/
