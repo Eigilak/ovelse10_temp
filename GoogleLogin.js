97679260f4a6fb40c9809a294fbfc2e8ffe7b312
Eigilak <48329669+Eigilak@users.noreply.github.com>
2020-10-04 14:53:41 +0200

    Create GoogleLogin.js

---
+++ GoogleLogin.js
@@ -0,0 +1,7 @@
+/*config indstillinger start */
+
+    iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
+    androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
+    iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
+    androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>'
+/**/
